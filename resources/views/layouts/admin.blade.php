64cd3356888ae3787b09a53e572a6918b6266c78
Luis <bugmannluis@gmail.com>
2025-08-11 07:24:32 +0100

    Primer commit - proyecto Laravel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Fontawesome -->
+         <script src="https://kit.fontawesome.com/af60fb507a.js" crossorigin="anonymous"></script>
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased bg-gray-50">
+      
+    
+@include('layouts.includes.admin.navigation')
+
+@include('layouts.includes.admin.sidebar')
+
+<div class="p-4 sm:ml-64">
+   <div class="mt-14">
+      {{ $slot }}
+</div>
+
+
+
+        @stack('modals')
+
+        @livewireScripts
+        <body>
+    @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+    </body>
+</html>
---
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -0,0 +1,27 @@
+@php
+   $links = [
+      [
+         'name' => 'Dashboard',
+         'icon' => 'fa-solid fa-gauge',
+         'href' => route('admin.dashboard'),
+         'active' => request()->routeIs('admin.dashboard')
+      ]
+   ];
+@endphp
+
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+   <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+      <ul class="space-y-2 font-medium">
+         @foreach($links as $link)
+         <li>
+            <a href="{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
+                  <i class="{{ $link['icon'] }}"></i>
+               </span>
+               <span class="ms-3">{{ $link['name'] }}</span>
+            </a>
+         </li>
+         @endforeach
+      </ul>
+   </div>
+</aside>
