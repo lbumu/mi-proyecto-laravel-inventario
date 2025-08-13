843d3d49c4280cec0c0a92d8c41652cb22e169df
Luis <bugmannluis@gmail.com>
2025-08-13 07:36:40 +0100

    13-08-2025 avance

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -6,11 +6,38 @@
          'href' => route('admin.dashboard'),
          'active' => request()->routeIs('admin.dashboard')
       ],
+      [
+         'Header' => 'Administrar pagina'
+      ],
         [
          'name' => 'Dashboard',
          'icon' => 'fa-solid fa-gauge',
          'href' => route('admin.dashboard'),
          'active' => false,
+         'submenu' => [
+            [
+               'name' => 'Products',
+               'icon' => 'fa-solid fa-box',
+               'href' => '#',
+               'active' => false
+            ],
+            [
+               'name' => 'Billing',
+               'icon' => 'fa-solid fa-credit-card',
+               'href' => '#',
+               'active' => false
+            ],
+            [
+               'name' => 'Invoice',
+               'icon' => 'fa-solid fa-file-invoice',
+               'href' => '#',
+               'active' => false
+
+
+
+            ]
+
+            ]
       ],
    ];
 @endphp
@@ -18,16 +45,45 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
-         @foreach($links as $link)
+   @foreach($links as $link)
+      @if(isset($link['Header']))
          <li>
-            <a href="{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
+            <div class="py-2 px-2 text-xs font-semibold text-gray-500 uppercase">
+               {{ $link['Header'] }}
+            </div>
+       
+      @else
+
+           @isset($link['submenu'])
+           </li>
+            <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                  <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
+                  <i class="{{ $link['icon'] }}"></i>
+               </span>
+                  <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{$link['name']}}</span>
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                  </svg>
+            </button>
+            <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                foreach($link['submenu'] as $item)
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
+                  </li>
+            </ul>
+           </li>
+           @else
+           @endisset
+           <a href="{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
                <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
                   <i class="{{ $link['icon'] }}"></i>
                </span>
                <span class="ms-3">{{ $link['name'] }}</span>
-            </a>
-         </li>
-         @endforeach
-      </ul>
+          </a>
+       
+      @endif
+   @endforeach
+
+</ul>
    </div>
 </aside>
