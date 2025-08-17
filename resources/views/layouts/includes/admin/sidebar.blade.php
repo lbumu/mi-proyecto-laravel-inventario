fc736c6b3ff14c10ed47a04b1215d6e89378bd56
Luis <bugmannluis@gmail.com>
2025-08-17 14:06:26 +0100

    avance 17-08

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -6,11 +6,26 @@
          'href' => route('admin.dashboard'),
          'active' => request()->routeIs('admin.dashboard')
       ],
+      [
+         'header' => 'Administrar Paginas'
+         ],
         [
-         'name' => 'Dashboard',
+         'name' => 'Chue 2',
          'icon' => 'fa-solid fa-gauge',
          'href' => route('admin.dashboard'),
          'active' => false,
+         'Submenu' => [
+            [
+               'name' => 'Submenu 1',
+               'href' => '#',
+               'active' => false,
+            ],
+            [
+               'name' => 'Submenu 2',
+               'href' => '#',
+               'active' => false,
+            ],
+         ]
       ],
    ];
 @endphp
@@ -20,14 +35,46 @@
       <ul class="space-y-2 font-medium">
          @foreach($links as $link)
          <li>
+             @isset($link['header'])
+            <div class="px-2 py-2 text-sm font-semibold text-gray-900 dark:text-white">
+               {{ $link['header'] }}
+            </div>
+             @else
+             @isset($link['Submenu'])
+             <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                  <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
+                  <i class="{{ $link['icon'] }}"></i>
+               </span>
+                  <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{$link['name']}}</span>
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                  </svg>
+            </button>
+            <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
+                  </li>
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
+                  </li>
+                  <li>
+                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
+                  </li>
+            </ul>
+
+                
+             @endisset
             <a href="{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
                <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
                   <i class="{{ $link['icon'] }}"></i>
                </span>
                <span class="ms-3">{{ $link['name'] }}</span>
             </a>
+               @endisset
          </li>
          @endforeach
+
+
       </ul>
    </div>
 </aside>
