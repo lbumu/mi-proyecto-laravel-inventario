d91e4e49f3136eab9162a71813befe627ab3dc79
Luis <bugmannluis@gmail.com>
2025-10-09 07:13:29 +0100

    crear productos de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,5 +22,8 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345678'),
         ]);
         $this->call([CategorySeeder::class]);
+
+        Product::factory(100)->create();
+
     }
 }
